ce6912ae4b08ac7340b5789359fbc459eda62f5c
SaranyaMeiyyappan <sharanyabecse@gmail.com>
2024-01-29 23:10:27 +0530

    added comments

--- src/main/java/com/saranya/LinkedList/ReverseLinkedList.java
+++ src/main/java/com/saranya/LinkedList/ReverseLinkedList.java
@@ -39,26 +39,31 @@ public class ReverseLinkedList {
 	}
 
 	private static Node recurse(Node head) {
+		// Base case:
+        // If the linked list is empty or has only one node, return the head as it is already reversed.
 		if(head == null || head.next == null) {
 			return head;
 		}
-		Node newHead = recurse(head.next);
-		Node front = head.next;
-		front.next = head;
-		head.next = null;
-		return newHead;
+		
+        // Recursive step:
+        // Reverse the linked list starting from the second node (head.next).
+		Node newHead = recurse(head.next); 
+		Node front = head.next; // Save a reference to the node following the current 'head' node.
+		front.next = head; // Make the 'front' node point to the current 'head' node in the reversed order.
+		head.next = null; // Break the link from the current 'head' node to the 'front' node to avoid cycles.
+		return newHead; // Return the 'newHead,' which is the new head of the reversed linked list.
 	}
 
 	private static Node solutionWithFlipTheOrder(Node head) {
-		Node temp = head;
-		Node prev = null;
-		while(temp!=null) {
-			Node front = temp.next;
-			temp.next = prev;
-			prev = temp;
-			temp = front;
+		Node temp = head; // Initialize'temp' at head of linked list
+		Node prev = null; // Initialize pointer 'prev' to NULL, representing the previous node
+		while(temp!=null) {  // Traverse the list, continue till 'temp' reaches the end (NULL)
+			Node front = temp.next; // Store the next node in 'front' to preserve the reference
+			temp.next = prev; // Reverse the direction of the current node's 'next' pointer to point to 'prev'
+			prev = temp; // Move 'prev' to the current node for the next iteration
+			temp = front; // Move 'temp' to the 'front' node advancing the traversal
 		}
-		return prev;
+		return prev;   // Return the new head of the reversed linked list
 	}
 
 }
